f172f16fcab1b3f668621039af32976635ce71fd
Jeremy Ward <jeremy@jmichaelward.com>
2021-03-27 09:37:48 -0500

    Add initial set of field properties.

--- src/Settings/Field.php
+++ src/Settings/Field.php
@@ -14,5 +14,103 @@ namespace JMichaelWard\GF_Structures\Settings;
  * @package JMichaelWard\GF_Structures\Settings
  */
 class Field {
+	/**
+	 * @var string
+	 */
+	private $type;
 
+	/**
+	 * @var string
+	 */
+	private $input_type;
+
+	/**
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @var string
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 */
+	private $label;
+
+	/**
+	 * @var bool
+	 */
+	private $required;
+
+	/**
+	 * @var string
+	 */
+	private $class;
+
+	/**
+	 * @var string
+	 */
+	private $tooltip;
+
+	/**
+	 * @var string
+	 */
+	private $tooltip_class;
+
+	/**
+	 * @var bool
+	 */
+	private $hidden;
+
+	/**
+	 * @var string
+	 */
+	private $default_value;
+
+	/**
+	 * @var bool
+	 */
+	private $horizontal;
+
+	/**
+	 * @var string|array
+	 */
+	private $dependency;
+
+	/**
+	 * @var array
+	 */
+	private $choices;
+
+	/**
+	 * @var string|array
+	 */
+	private $feedback_callback;
+
+	/**
+	 * @var array
+	 */
+	private $callback;
+
+	/**
+	 * @var string|array
+	 */
+	private $validation_callback;
+
+	/**
+	 * @var string
+	 */
+	private $after_input;
+
+	/**
+	 * @var array
+	 */
+	private $field_map;
+
+	/**
+	 * @var array
+	 */
+	private $html_attributes;
 }
