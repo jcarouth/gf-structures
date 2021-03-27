0ecfe0af2ee2d475f72ca6c723bfc246826a2f3d
Jeremy Ward <jeremy@jmichaelward.com>
2021-03-27 09:20:14 -0500

    Create src directory and add initial classes for settings section and field types.

---
+++ src/Settings/Field.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Object for registering a settings field.
+ *
+ * @see https://docs.gravityforms.com/category/developers/php-api/add-on-framework/settings-api/#field-properties
+ * @package JMichaelWard\GF_Structures\Settings
+ */
+
+namespace JMichaelWard\GF_Structures\Settings;
+
+/**
+ * Class Field
+ *
+ * @package JMichaelWard\GF_Structures\Settings
+ */
+class Field {
+
+}
---
+++ src/Settings/Section.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Object for rendering a settings section.
+ *
+ * @see https://docs.gravityforms.com/category/developers/php-api/add-on-framework/settings-api/#creating-a-plugin-settings-page
+ * @package JMichaelWard\GF_Structures\Settings
+ */
+
+namespace JMichaelWard\GF_Structures\Settings;
+
+/**
+ * Class Section
+ *
+ * @package JMichaelWard\GF_Structures\Settings
+ */
+class Section {
+
+}
